f514c4db2219b1942efbf3a09923804f2fd67711
Agustin <agustin.roveta97@gmail.com>
2023-04-04 15:59:33 -0300

    a

--- Preferencias de Usuario/preferencias_usuario/lib/providers/theme_provider.dart
+++ Preferencias de Usuario/preferencias_usuario/lib/providers/theme_provider.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 //*no son un gestor de estado, lo cual impide que podamos cambiar en el momento, sin
 //*reiniciar la aplicación, las preferencias de usuario se utilizan para grabar
 //*informacion en el dispositivo, como hacemos en preferencias.dart
-//*Este gestor de estado (provider) nos permite hacer el cambio de theme instantaneamente
+//*Este gestor de estado (provider) nos permite hacer el cambio de
+//* theme instantaneamente y lo mantiene en el dispositivo,
+//* osea que no se pierde si reiniciamos la app o hacemos hot reload
+
 class ThemeProvider extends ChangeNotifier {
   ThemeData currentTheme;
   ThemeProvider({required bool isDarkmode})
